5594f1e3d535e0dd27141d7f30653078590bce63
Aelita <xsjctony@126.com>
2023-10-02 22:18:34 +1100

    Project Init

---
+++ uno.config.ts
@@ -0,0 +1,27 @@
+import presetRemToPx from '@unocss/preset-rem-to-px'
+import { defineConfig, presetUno } from 'unocss'
+import type { Theme } from '@unocss/preset-uno'
+import type { UserShortcuts } from 'unocss'
+
+
+type NFTheme = typeof theme
+
+
+const theme = {} satisfies Theme
+
+
+const shortcuts = [
+  // UTILITIES
+  ['absolute-center', 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2', { layer: 'utilities' }]
+] satisfies UserShortcuts<NFTheme>
+
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    // this will convert rem to px, e.g. `p-1` now means `padding: 1px` instead of `0.25rem`(=4px)
+    presetRemToPx({ baseFontSize: 4 })
+  ],
+  shortcuts,
+  theme
+})
